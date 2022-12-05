ac8810583a1d0f1be26c2473fae4f202af913948
Ikeda_Tomomi <abcd.y327.s2@gmail.com>
2022-12-05 09:33:33 +0900

    update : ペイント機能

--- WebApp/js/paint-page.js
+++ WebApp/js/paint-page.js
@@ -32,7 +32,7 @@ $(function () {
         MY_BURASISIZE = input_val;
     });
 
-    $('canvas').on('touchstart', function (event) {
+    $('#canvas').on('touchstart', function (event) {
         // 画面がタッチされたときの処理を記述する
         event.preventDefault();
         var pageX = event.originalEvent.touches[0].pageX;
@@ -43,7 +43,7 @@ $(function () {
         startX = point.x;
         startY = point.y;
     });
-    $('canvas').on('touchmove', function (event) {
+    $('#canvas').on('touchmove', function (event) {
         // 画面がタッチされながら移動したときの処理を記述する
         event.preventDefault();
         var pageX = event.originalEvent.touches[0].pageX;
@@ -63,6 +63,11 @@ $(function () {
         startX = endX;
         startY = endY;
     });
+
+    $('#canvas').on('touchend', function (event) {
+        saveImageData()
+    })
+
     // 画面のx,y座標からcanvasのx,y座標に変換する
     function getCanvasPoint(pageX, pageY) {
         var base = canvas.getBoundingClientRect();
@@ -89,6 +94,53 @@ $(function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
     });
 
+    var imageMemory = new Array(11); // キャンバスのイメージの保存用配列
+    var flagMemory = 0; // 現在のキャンバスの番号
+    // $('#backBtn, #forwardBtn').attr('disabled', true);
+    // 現在のキャンバス状態を保存
+    function saveImageData() {
+        // 現在の状態を保存
+        if (flagMemory == imageMemory.length - 1) {
+            imageMemory.shift();
+        } else {
+            ++flagMemory;
+        }
+
+        if (flagMemory == imageMemory.length - 1) {
+            $('#forwardBtn').attr('disabled', true);
+        }
+
+        imageMemory[flagMemory] = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        $('#backBtn').attr('disabled', false);
+    }
+    // 戻るボタン
+    $('#backBtn').click(function () {
+
+        if (flagMemory > 0) {
+            flagMemory--;
+            context.putImageData(imageMemory[flagMemory], 0, 0);
+
+            $('#forwardBtn').attr('disabled', false);
+            if (flagMemory == 0) {
+                $('#backBtn').attr('disabled', true);
+            }
+        }
+    });
+    // 進むボタン
+    $('#forwardBtn').click(function () {
+        if (flagMemory < imageMemory.length - 1) {
+            flagMemory++;
+            context.putImageData(imageMemory[flagMemory], 0, 0);
+
+            $('#backBtn').attr('disabled', false);
+            if (flagMemory == imageMemory.length - 1) {
+                $('#forwardBtn').attr('disabled', true);
+            }
+        }
+
+    });
+
     // ペイントから送信
     $("#paint_finish").on("click", function () {
         console.log(ImageProc.toBinaryData(context.getImageData(0, 0, w, h).data, 2))
@@ -97,14 +149,14 @@ $(function () {
         $.ajax({
             url: '/data',
             type: 'POST',
-            data: {'send_data':send_data},
+            data: {
+                'send_data': send_data
+            },
             // processData: false,
             // contentType: 'text/plain',
             dataType: 'text',
-            success: function (data) {
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-            }
+            success: function (data) {},
+            error: function (XMLHttpRequest, textStatus, errorThrown) {}
         });
     })
 })
