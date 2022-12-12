3ecf0cbbd92dc0fd4303524f1ad54c32c71be928
Ikeda_Tomomi <abcd.y327.s2@gmail.com>
2022-12-12 11:14:02 +0900

    update

--- WebApp/js/QRcode.js
+++ WebApp/js/QRcode.js
@@ -1,20 +1,16 @@
-// $(function () {
-// let w = 70;
-// let h = 70;
-// // QRコード生成
-// $('.qr').qrcode({
-//     // render: "canvas",
-//     width: w,
-//     height: h,
-//     text: "this is test."
-// });
+function getQrPosition() {
+    let w = 80;
+    let h = 80;
+    // QRコード生成
+    $('.qr').qrcode({
+        // render: "canvas",
+        width: w,
+        height: h,
+        text: "this is test."
+    });
 
-// class QRcode {
-//     // QRコードの座標取得
-//     getQrPosition() {
-//         var qr_canvas = $(".qr canvas");
-//         var qr_context = qr_canvas.getContext("2d");
-//         // console.log(qr_context.getImageData(0, 0, w, h).data, 2);
-//         console.log(ImageProc.toBinaryData(qr_context.getImageData(0, 0, w, h).data, 2))
-//     };
-// }
+    var qr_canvas = $(".qr canvas");
+    console.log(qr_canvas);
+    var qr_context = qr_canvas[1].getContext("2d");
+    console.log(ImageProc.toBinaryData(qr_context.getImageData(0, 0, w, h).data, 2));
+};
--- WebApp/js/paint-page.js
+++ WebApp/js/paint-page.js
@@ -136,23 +136,6 @@ $(function () {
         }
     });
 
-    function getQrPosition() {
-        let w = 70;
-        let h = 70;
-        // QRコード生成
-        $('.qr').qrcode({
-            // render: "canvas",
-            width: w,
-            height: h,
-            text: "this is test."
-        });
-
-        var qr_canvas = $(".qr canvas");
-        console.log(qr_canvas);
-        var qr_context = qr_canvas[1].getContext("2d");
-        console.log(ImageProc.toBinaryData(qr_context.getImageData(0, 0, w, h).data, 2));
-    };
-
     // ペイントから送信
     $("#paint_finish").on("click", function () {
         getQrPosition()
